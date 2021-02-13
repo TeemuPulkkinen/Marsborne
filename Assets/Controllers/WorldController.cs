1b084cd7345f7d8c08c94bdbda38b0a97ed54c5e
Teemu Pulkkinen <pulkkinen.teemu@gmail.com>
2021-02-13 09:12:23 +0200

    Add functionalities for creating the world

---
+++ Assets/Controllers/WorldController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldController : MonoBehaviour
+{
+    World world;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        world = new World();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
